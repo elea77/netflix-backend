719d77b750a3408f66beada0e56f1ac3882070b2
eleacarton <eleacarton@me.com>
2022-02-04 12:08:05 +0100

    modif email

--- src/middlewares/validators/users.validation.js
+++ src/middlewares/validators/users.validation.js
@@ -4,7 +4,7 @@ const userSchemaValidation = (req, res, next) => {
     const userValidationSchema = Joi.object({
         firstName: Joi.string().required(),
         lastName: Joi.string().required(),
-        email: Joi.string().email(),
+        email: Joi.string().email().required(),
         password: Joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{6,64}$')).required(),
         isAdmin: Joi.boolean(),
         abonnement: Joi.string().required()
