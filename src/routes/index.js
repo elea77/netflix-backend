f659472cb19d9fa9870732167054dab4bede6065
eleacarton <eleacarton@me.com>
2022-02-20 18:36:59 +0100

    desactive

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,15 +5,15 @@ const userRouter = require('./user.route');
 const movieRouter = require('./movie.route');
 const categoryRouter = require('./category.route');
 const wishlistRouter = require('./wishlist.route');
-const checkoutRouter = require('./checkout.route');
-const webHooksRouter = require('./webhook.route');
+// const checkoutRouter = require('./checkout.route');
+// const webHooksRouter = require('./webhook.route');
 
 router.use(userRouter);
 router.use(movieRouter);
 router.use(categoryRouter);
 router.use(wishlistRouter);
-router.use(checkoutRouter);
-router.use(webHooksRouter);
+// router.use(checkoutRouter);
+// router.use(webHooksRouter);
 
 
 module.exports = router;
